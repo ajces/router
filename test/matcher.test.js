2553700033f0d55acc0f885a029ad99f15c03ec9
Andy Johnson <andyrjohnson82@gmail.com>
2017-09-21 14:41:36 -0700

    coverage on updateMeta should be higher not sure why 39,41,42,44 are not covered with current tests...

--- test/matcher.test.js
+++ test/matcher.test.js
@@ -1,5 +1,7 @@
 import test from "ava";
-import { Matcher } from "../src/matcher";
+import { Matcher, updateMeta } from "../src/matcher";
+
+require("undom/register");
 
 const defaultMeta = { keywords: "one, two, three" };
 const config = [
@@ -24,6 +26,57 @@ const config = [
     meta: { title: "404" }
   }
 ];
+
+function buildTestMeta() {
+  document.head = document.createElement("head");
+  Object.keys(defaultMeta).forEach(key => {
+    const newMeta = document.createElement("meta");
+    newMeta.setAttribute("name", key);
+    newMeta.setAttribute("content", defaultMeta[key]);
+    document.head.appendChild(newMeta);
+  });
+}
+
+document.getElementsByTagName = name => {
+  if (name === "meta") {
+    return document.head.childNodes;
+  } else {
+    return undefined;
+  }
+};
+
+function testMeta(t, expected) {
+  document.head.childNodes.forEach(node => {
+    let name = node.name;
+    let content = node.content;
+    t.is(content, expected[name]);
+  });
+}
+
+test("updateMeta should properly diff/patch meta tags...", t => {
+  buildTestMeta();
+  testMeta(t, defaultMeta);
+  const changedMeta = {
+    author: "Andy Johnson",
+    keywords: "a b c"
+  };
+  updateMeta(changedMeta);
+  testMeta(t, changedMeta);
+  const removeMeta = {
+    author: "Jon Doe"
+  };
+  updateMeta(removeMeta);
+  testMeta(t, removeMeta);
+  const invalidMeta = {
+    origin: "bam",
+    referrer: "pow",
+    viewport: "bang"
+  };
+  updateMeta(removeMeta);
+  testMeta(t, removeMeta);
+  updateMeta({});
+  testMeta(t, {});
+});
 
 test("matcher should return proper route object", t => {
   const matcher = Matcher(config, defaultMeta);
